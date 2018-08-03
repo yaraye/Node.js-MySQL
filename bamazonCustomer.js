fcbf875702f9a152edb592dacedd2046b3a16c85
yaraye <yaraye@gmail.com>
2018-08-02 23:26:40 -0400

    Add files via upload

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -2,6 +2,7 @@ var mysql = require ('mysql');
 var inquirer = require ('inquirer');
 require("dotenv").config(); 
 
+
 var connection = mysql.createConnection({
     host: process.env.HOST,
     user: process.env.USERNAME,
@@ -10,70 +11,25 @@ var connection = mysql.createConnection({
     database:'bamazonDB'
   });
   
-
 connection.connect(function(err) {
     if (err) throw err;
-    displayProducts();
+    connection.query("SELECT * FROM products", function(err, result) {
+        for (var i=0; i< result.length; i++){
+            console.log("ID:" +""+ result[i].id);
+            console.log("product Name:" +""+ result[i].product_name);
+            console.log("Department Name:" +""+ result[i].department_name);
+            console.log("Price:"+""+ result[i].price);
+            console.log("Stock Quantity:" +""+ result[i].stock_quantity);
+            // console.log(result);
+        }
+        idsearch();
   });
-
-  function displayProducts(){
-      inquirer.prompt({
-        name: "action",
-        type: "list",
-        message: "What would you like to do?",
-        choices: [
-            "what items would you like to search?", 
-            "what ID of the product would like to buy?", 
-            "How many units of the product would like to buy?"
-        ] }).then(function (answer){
-          switch(answer.action){
-              case  "what items would you like to search?":
-              itemSearch ();
-              break;
-
-              case  "what ID of the product would like to buy?":
-              idsearch();
-              break;
-
-              case  "How many units of the product would like to buy?":
-                productSearch();
-                break;
-
-          }
-      })
-  }
-  
-
- function itemSearch (){
-    inquirer.prompt({
-        name:"id",
-        type:"input",
-        message:" what items would you like to search?"
-    }).then(function(answer){
-        var query= "SELECT * FROM products Where?";
-        // display all of the items available for sale
-        connection.query(query,{ product_name: answer.id }, function(err, result) {
-
-            for (var i=0; i< result.length; i++){
-                console.log("ID:" +""+ result[i].id);
-                console.log("product Name:" +""+ result[i].product_name);
-                console.log("Department Name:" +""+ result[i].department_name);
-                console.log("Price:"+""+ result[i].price);
-                console.log("Stock Quantity:" +""+ result[i].stock_quantity);
-                
-            }
-            displayProducts();
-                
-        })
-    })
-}
-
-
+})
 
 function idsearch(){inquirer.prompt({
-    name:"number",
+    name:"id",
     type:"input",
-    message:"what ID of the product would like to buy?",
+    message:" what items ID would you like to search?",
     validate: (value) => {
         let valid = !isNaN(parseFloat(value));
         return valid || 'Please enter a number'
@@ -81,7 +37,7 @@ function idsearch(){inquirer.prompt({
 }).then(function(answer){
     //  The first should ask them the ID of the product they would like to buy.
 var query = "SELECT * FROM products Where?";
-connection.query(query, {id:answer.number}, function(err, result){
+connection.query(query, {id:answer.id}, function(err, result){
     for (var i=0; i< result.length; i++){
         console.log("ID:" +""+ result[i].id);
         console.log("product Name:" +""+ result[i].product_name);
@@ -89,33 +45,42 @@ connection.query(query, {id:answer.number}, function(err, result){
         console.log("Price:"+""+ result[i].price);
         console.log("Stock Quantity:" +""+ result[i].stock_quantity);
     }
-    displayProducts();
-})
-})
+ 
+    inquirer.prompt([{
+        name:"stock_quantity",
+        type:"input",
+        message:"How many units of the product would like to buy?",
+        validate: (value) => {
+            let valid = !isNaN(parseFloat(value));
+            return valid || 'Please enter a number'
+        }
 
-}
-
-//  The second message should ask how many units of the product they would like to buy.
-function productSearch(){
-    inquirer.prompt({
-    name:"number",
-    type:"input",
-    message:"How many units of the product would like to buy?",
-    validate: (value) => {
-        let valid = !isNaN(parseFloat(value));
-        return valid || 'Please enter a number'
-    }
-}).then(function(answer){
-    //  The first should ask them the ID of the product they would like to buy.
-var query = "SELECT * FROM products Where?";
-connection.query(query, {id:answer.number}, function(err, result){
-    for (var i=0; i< result.length; i++){
-        console.log("ID:" +""+ result[i].id);
-        console.log("product Name:" +""+ result[i].product_name);
-        console.log("Stock Quantity:" +""+ result[i].stock_quantity);
-    }
-    displayProducts();
+}]).then(function(answer){
+    var quantity = answer.stock_quantity;
+    var itemId = answer.id;
+// connection.query(query, {stock_quantity: answer.stock_quantity},function(err, result){
+    // console.log(result);
+        for (var i=0; i< result.length; i++){
+            if (result[i].stock_quantity - quantity <= 0 ){
+                console.log("Insufficient quantity! Currently we only have "+ result[i].stock_quantity+ result[i].product_name);
+            }else
+                console.log("ID:" +""+ result[i].id);
+                console.log("product Name:" +""+ result[i].product_name);
+                console.log("Stock Quantity:" +""+ result[i].stock_quantity);
+                console.log("You total is"+(result[i].stock_quantity *result[i].price));
+    
+    connection.query('UPDATE products SET stock_quantity=? WHERE id=?', [result[i].stock_quantity - quantity, itemId],
+                function(err, result) {
+                    if (err) throw err;
+    
+                    // displayProducts();
+                }); 
+           
+        }
+        // displayProducts();
+    // })
+  
 })
 })
-
+});
 }
